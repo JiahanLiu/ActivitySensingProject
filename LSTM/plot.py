2e36e99c69c291615baef0ea7d3bb065bce83212
Jiahan Liu <jiahan.jim.liu@gmail.com>
2018-12-06 05:20:03 +0000

    hi

--- LSTM/plot.py
+++ LSTM/plot.py
@@ -13,15 +13,18 @@ codeFilename = "bacc_metric_simple.pkl";
 fig = plt.figure()
 ax = fig.add_subplot(111)
 
-with open(os.path.join(dirs, codeFilename), 'rb') as input:  # Overwrites any existing file.
-	metric_simple = pickle.load(input)
-	i = 1
-	single_run_x = []
-	single_run_y = []
-	for x in metric_simple:
-		single_run_x.append(i)
-		single_run_y.append(x[3])
-		i = i + 1
-		
-ax.scatter(x=single_run_x, y=single_run_y, label='linear')
-fig.savefig('test.png')
+for dirs in subdirs:
+	with open(os.path.join(dirs, codeFilename), 'rb') as input:  # Overwrites any existing file.
+		metric_simple = pickle.load(input)
+		i = 1
+		single_run_x = []
+		single_run_y = []
+		for x in metric_simple:
+			single_run_x.append(i)
+			single_run_y.append(x[3])
+			i = i + 1
+
+	plt.xlabel('epoch')
+	plt.ylabel('balanced accuracy')		
+	ax.scatter(x=single_run_x, y=single_run_y, label='linear')
+	fig.savefig(dirs + '.png')
