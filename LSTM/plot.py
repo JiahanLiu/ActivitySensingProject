415fe185a0c1bf14c28957fb1c949ede6d792811
Jiahan Liu <jiahan.jim.liu@gmail.com>
2018-12-06 01:36:13 +0000

    LSTM stuff

--- LSTM/plot.py
+++ LSTM/plot.py
@@ -10,17 +10,23 @@ for dirs in allFilesInDir:
 
 codeFilename = "bacc_metric_simple.pkl";
 
-with open(os.path.join('lstm_prediction_stacked_thick_dropout_3/', codeFilename), 'rb') as input:  # Overwrites any existing file.
-	metric_simple = pickle.load(input)
-	i = 1
-	single_run_x = []
-	single_run_y = []
-	for x in metric_simple:
-		single_run_x.append(i)
-		single_run_y.append(x[3])
-		i = i + 1
-
 fig = plt.figure()
 ax = fig.add_subplot(111)
-ax.scatter(x=single_run_x, y=single_run_y, label='linear')
-fig.savefig('test.png')
+
+runs_y = []
+
+for dirs in subdirs:
+	with open(os.path.join(dirs, codeFilename), 'rb') as input:  # Overwrites any existing file.
+		metric_simple = pickle.load(input)
+		i = 1
+		single_run_x = []
+		single_run_y = []
+		for x in metric_simple:
+			single_run_x.append(i)
+			single_run_y.append(x[3])
+			i = i + 1
+		runs_y.append(single_run_y)
+
+print(runs_y)
+# ax.scatter(x=single_run_x, y=single_run_y, label='linear')
+# fig.savefig('test.png')
